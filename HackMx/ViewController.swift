f8ff9f05f951aaf2442f3e7cd4c9972b01117b8b
Peredo <peredo98@hotmail.com>
2019-04-27 10:54:55 -0500

    create project

---
+++ HackMx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackMx
+//
+//  Created by Emiliano Peredo on 4/27/19.
+//  Copyright © 2019 Servebeer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
